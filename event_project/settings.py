d7428a85d35856024737c9ff09002c633105f210
vasuki-sr <vasukisr2002@gmail.com>
2026-07-05 21:50:22 +0530

    Add STATIC_ROOT

--- event_project/settings.py
+++ event_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-dn0@r%ri)4l#t#p+*y$yu--oz&dlbyj5y2ivlau+3)ma&h79@c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
